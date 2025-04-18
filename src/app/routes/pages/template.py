c7abee8b98a5ae254727d36c23d6e4e67c999853
WAI-Forward <148784381+WAI-Forward@users.noreply.github.com>
2025-04-18 03:51:26 +0100

    Update template.py

--- src/app/routes/pages/template.py
+++ src/app/routes/pages/template.py
@@ -13,7 +13,7 @@ src/app/templates/pages/<page>.html
 """
 
 from flask import render_template
-from . import pages_bp  # This is the Blueprint used for all page routes
+from . import pages_bp  # This is the Blueprint used for all page routes, has prefix /page
 
 # 👇 GPT should update this line:
 @pages_bp.route("/template")
